9d3c6b68449c00dd7e3c9655b709bee1ac4b025a
Jatin <jatinverma2428@gmail.com>
2025-05-29 12:18:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("btn")
+let body=document.body;
+button.addEventListener("click",() => {
+
+    body.style.backgroundColor="#bbbff1";
+});
+button.addEventListener("dblclick",()=>{
+     body.style.backgroundColor="black";
+})
